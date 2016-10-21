9842b150bc4732f83c6ebd758cbf881eae2f2b76
Konstantin Rudenko <k.rudenko@foodpanda.com>
2016-10-22 00:48:36 +0200

    new_design make base class abstract

--- app/src/main/java/com/w2mem/app/BaseActivity.java
+++ app/src/main/java/com/w2mem/app/BaseActivity.java
@@ -2,5 +2,10 @@ package com.w2mem.app;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+/**
+ * Base class where we can save fragment manager
+ * and add crash check tool for instance
+ * //TODO add fragment manager
+ */
+public abstract class BaseActivity extends AppCompatActivity {
 }
